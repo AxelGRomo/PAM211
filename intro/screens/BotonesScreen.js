fa680a1ccf36126b438d6a2cdbf022bce26cd9bc
AxelGRomo <124051240@upq.edu.mx>
2025-10-16 23:23:44 -0600

    Practica 8
    Se creo un archivo BotonesScreen el cual nos permitio ver el tema de creacion de botones personalizados.

---
+++ intro/screens/BotonesScreen.js
@@ -0,0 +1,78 @@
+import { Text, StyleSheet, View, TouchableOpacity, Switch } from 'react-native';
+import React, { useState } from 'react';
+
+export default function BotonesScreen() {
+  const [prendido, setPrendido] = useState(false);
+  const backgroundColor = prendido ? '#F5F5F5' : '#121212';
+  const textColor = prendido ? '#000000' : '#FFFFFF';
+
+  return (
+    <View style={[styles.container, {backgroundColor}]}>
+      <Text style={[styles.texto, {color: textColor}]}>Estado: {prendido ? 'Prendido' : 'Apagado'}</Text>
+
+      <TouchableOpacity
+        style={styles.botonEncendido}
+        onPress={() => setPrendido(true)}>
+        <Text style={styles.textoBoton}>PRENDER</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.botonApagado}
+        onPress={() => setPrendido(false)}>
+        <Text style={styles.textoBoton}>APAGAR</Text>
+      </TouchableOpacity>
+
+      {/* Switch bien ubicado */}
+      <View style={styles.switchContainer}>
+        <Text style={[styles.switchLabel, {color: textColor}]}>Control de Switch:</Text>
+        <Switch value={prendido} onValueChange={setPrendido} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#B77272',
+  },
+  texto: {
+    fontSize: 22,
+    marginBottom: 30,
+    fontWeight: 'bold',
+    color: '#fff',
+  },
+  botonEncendido: {
+    backgroundColor: '#0C9600',
+    paddingVertical: 12,
+    paddingHorizontal: 35,
+    borderRadius: 10,
+    marginBottom: 15,
+  },
+  botonApagado: {
+    backgroundColor: '#B10000',
+    paddingVertical: 12,
+    paddingHorizontal: 35,
+    borderRadius: 10,
+    marginBottom: 25,
+  },
+  textoBoton: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  switchLabel: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginRight: 10,
+  },
+});
